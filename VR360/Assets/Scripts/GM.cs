1548793183a9c2303b692784c1dcf0c11d8d052e
KID <50666207+KID421@users.noreply.github.com>
2021-04-23 16:43:35 +0800

    平面影片跟靜音

--- VR360/Assets/Scripts/GM.cs
+++ VR360/Assets/Scripts/GM.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class GM : MonoBehaviour
 {
     public int score;
     public Text uiScore;
 
+    public VideoPlayer player360;
+    public VideoPlayer player2D;
+
+    private bool mute;
+
     public void AddScore()
     {
         score += 10;
         uiScore.text = "分數：" + score;
+    }
+
+    public void SwitchMute()
+    {
+        mute = !mute;                           // 靜音相反
+        player360.SetDirectAudioMute(0, mute);  // 360
+        player2D.SetDirectAudioMute(0, !mute);  // 2D
     }
 }
