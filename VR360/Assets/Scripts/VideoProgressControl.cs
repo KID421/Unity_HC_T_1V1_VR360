0411e24fafcbe348d3594f39b8b7244375d396f2
KID <50666207+KID421@users.noreply.github.com>
2022-05-04 17:42:17 +0800

    Update 添加進度條與影片控制按鈕

---
+++ VR360/Assets/Scripts/VideoProgressControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoProgressControl : MonoBehaviour
+{
+    [Header("放 360 影片")]
+    public VideoPlayer video;
+    [Header("放介面內的 Slider 進度條")]
+    public Slider sliderVideo;
+
+    public void SliderControl(float value)
+    {
+        // print("進度條的值：" + value);
+
+        print("frame " + video.frame);
+        print("frame count " + video.frameCount);
+
+        long current = (long)(video.frameCount * value);
+        print("運算完的當前：" + current);
+
+        video.frame = current;
+        video.Play();
+    }
+
+    public void Play()
+    {
+        video.Play();
+    }
+
+    public void Pause()
+    {
+        video.Pause();
+    }
+
+    public void Stop()
+    {
+        video.Stop();
+    }
+}
