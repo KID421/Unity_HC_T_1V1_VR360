66b3c6f0ae3d05d81e509dbe2599ee7c951e8537
KID <50666207+KID421@users.noreply.github.com>
2022-05-30 17:34:39 +0800

    Update 添加控制 2D 影片進度

--- VR360/Assets/Scripts/VideoProgressControl.cs
+++ VR360/Assets/Scripts/VideoProgressControl.cs
@@ -9,18 +9,23 @@ public class VideoProgressControl : MonoBehaviour
     [Header("放介面內的 Slider 進度條")]
     public Slider sliderVideo;
 
+    [Header("放 2D 影片")]
+    public VideoPlayer video2D;
+
     public void SliderControl(float value)
     {
         // print("進度條的值：" + value);
-
-        print("frame " + video.frame);
-        print("frame count " + video.frameCount);
+        // print("frame " + video.frame);
+        // print("frame count " + video.frameCount);
 
         long current = (long)(video.frameCount * value);
-        print("運算完的當前：" + current);
-
+        // print("運算完的當前：" + current);
         video.frame = current;
         video.Play();
+
+        long current2D = (long)(video2D.frameCount * value);
+        video2D.frame = current2D;
+        video2D.Play();
     }
 
     public void Play()
