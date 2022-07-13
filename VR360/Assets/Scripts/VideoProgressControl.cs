4a47cd2d0328fe5528561af15655065352d9a1bb
KID <50666207+KID421@users.noreply.github.com>
2022-07-13 17:41:45 +0800

    Update 優化滑桿、按鈕並調整介面至控制器上

--- VR360/Assets/Scripts/VideoProgressControl.cs
+++ VR360/Assets/Scripts/VideoProgressControl.cs
@@ -6,11 +6,36 @@ public class VideoProgressControl : MonoBehaviour
 {
     [Header("放 360 影片")]
     public VideoPlayer video;
+    [Header("放 2D 影片")]
+    public VideoPlayer video2d;
     [Header("放介面內的 Slider 進度條")]
     public Slider sliderVideo;
 
     [Header("放 2D 影片")]
     public VideoPlayer video2D;
+
+    [Header("所有影片")]
+    public VideoControl[] videosControls;
+    [Header("所有影片按鈕")]
+    public Button[] btnsVideos;
+
+    private void Awake()
+    {
+        InvokeRepeating("UpdateVideosButton", 0, 1);
+    }
+
+    public void UpdateVideosButton()
+    {
+        for (int i = 0; i < videosControls.Length; i++)
+        {
+            if (btnsVideos[i].interactable) continue;
+
+            if (Time.timeSinceLevelLoad > videosControls[i].playSecond)
+            {
+                btnsVideos[i].interactable = true;
+            }
+        }
+    }
 
     public void SliderControl(float value)
     {
@@ -26,20 +51,39 @@ public class VideoProgressControl : MonoBehaviour
         long current2D = (long)(video2D.frameCount * value);
         video2D.frame = current2D;
         video2D.Play();
+
+        // 重複執行 影片數量，有六部就執行六次
+        for (int i = 0; i < videosControls.Length; i++)
+        {
+            // 如果 當前影格數 > 影片[i] 的 時間
+            if (current > videosControls[i].playSecond * 60)
+            {
+                // print("影片時間：" + videosControls[i].playSecond);
+
+                btnsVideos[i].interactable = true;
+            }
+            else
+            {
+                btnsVideos[i].interactable = false;
+            }
+        }
     }
 
     public void Play()
     {
         video.Play();
+        video2d.Play();
     }
 
     public void Pause()
     {
         video.Pause();
+        video.Pause();
     }
 
     public void Stop()
     {
+        video.Stop();
         video.Stop();
     }
 }
